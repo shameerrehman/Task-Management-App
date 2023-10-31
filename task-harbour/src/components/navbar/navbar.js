abc6923acee40c45fe26e06867543b314bb5cca0
unknown <atheesh.k28@gmail.com>
2023-10-30 21:27:33 -0400

    Navbar with all functionality: login and project list

--- task-harbour/src/components/navbar/navbar.js
+++ task-harbour/src/components/navbar/navbar.js
@@ -3,6 +3,12 @@ import './navbar.css';
 
 function NavBar() {
     const location = useLocation();
+    let signedIn = true;
+    const projects = [
+        { 'name': 'Project 1', 'url': 'project1', 'color': 'red' },
+        { 'name': 'Project 2', 'url': 'project2', 'color': 'green' },
+        { 'name': 'Project 3', 'url': 'project3', 'color': 'blue' }
+    ]
     return (
         <div class="navbar">
             <div class="dropdown">
@@ -12,33 +18,61 @@ function NavBar() {
                     <div class="div-icon"></div>
                 </button>
                 <div class="dropdown-content">
-                    <li class="newProject">
-                        <NavLink
-                            to="/"
-                            className={`nav-links${location.pathname === "/" ? " activated" : "" }`}
-                        // onClick={closeMobileMenu}
-                        > New Project
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/"
-                            className={`nav-links${location.pathname === "/" ? " activated" : "" }`}
-                        // onClick={closeMobileMenu}
-                        > Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/signup"
-                            className={`nav-links${location.pathname === "/signup" ? " activated" : "" }`}
-                        // onClick={closeMobileMenu}
-                        >
-                            signup
-                        </NavLink>
-                    </li>
-                    
-                        
+                    <h2>
+                    Task Harbor
+                    </h2>
+                    {/* Not Signed In Navbar */}
+                    {!signedIn && (<>
+                        <li>
+                            <NavLink to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/signup">
+                                Sign Up
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login">
+                                Login
+                            </NavLink>
+                        </li>
+                    </>)}
+                    {/* Signed In Navbar */}
+                    {signedIn && (<>
+                        <li >
+                            <NavLink to="/">
+                                <div class="newProject">New Project</div>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/projects">
+                                Projects
+                            </NavLink>
+                            {(() => {
+                                let projectsLinks = [];
+                                projects.forEach(project => {
+                                    let urllink = "/projects/" + project.url
+                                    let color = project.color
+                                    projectsLinks.push(<li class="projectlink"><NavLink to={urllink} ><div class="nameAndDot"><span style={{ backgroundColor:color }} class="dot"/> {project.name}</div></NavLink></li>)
+                                });
+                                return projectsLinks
+                            })()}
+                        </li>
+                        <li>
+                            <NavLink to="/logout">
+                                Logout
+                            </NavLink>
+                        </li>
+                    </>)}
+
+
                 </div>
             </div>
         </div>
