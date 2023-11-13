27ff110477bb19e545915022f3722707c2388d7f
shruthigudimalla <146785514+shruthigudimalla@users.noreply.github.com>
2023-11-12 20:46:26 -0500

    Update navbar.js

--- task-harbour/src/components/navbar/navbar.js
+++ task-harbour/src/components/navbar/navbar.js
@@ -1,121 +1,88 @@
 import { NavLink, useLocation } from "react-router-dom";
 import './navbar.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function NavBar() {
     const location = useLocation();
-    const [projectsList, setProjectsList] = useState()
+    const [projectsList, setProjectsList] = useState([]);
 
-    function projectHelper() {
-        if (projectsList) {
-            return
+    useEffect(() => {
+        const authData = JSON.parse(localStorage.getItem("authData"));
+        if (authData && authData.username) {
+            fetchProjects(authData.username);
         }
-        const formData = { userID: JSON.parse(localStorage.getItem("authData")).username }
+    }, [location]);
+
+    function fetchProjects(username) {
+        const formData = { userID: username };
         fetch('https://rsf6bjt4de6goirsyfvxfs2zdy0vquva.lambda-url.us-east-1.on.aws/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(formData),
-        }).then(response => {
-            return response.json()
-        }).then(data => {
-            let colorDict = {}
-            data['data'].forEach(project => {
-                var letters = '0123456789ABCDEF';
-                var color = '#';
-                for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                }
-                colorDict[project.projectID] = color
-            })
-            data['color'] = colorDict
-            setProjectsList(data)
         })
+        .then(response => response.json())
+        .then(data => {
+            let colorDict = {};
+            data['data'].forEach(project => {
+                var color = '#' + Math.floor(Math.random()*16777215).toString(16);
+                colorDict[project.projectID] = color;
+            });
+            data['color'] = colorDict;
+            setProjectsList(data['data']);
+        });
     }
-    function loginHelper() {
+
+    function isLoggedIn() {
         try {
-            let token = JSON.parse(localStorage.getItem("authData")).authInfo.AccessToken
-            projectHelper()
-            return true
+            const authData = JSON.parse(localStorage.getItem("authData"));
+            return authData && authData.authInfo && authData.authInfo.AccessToken;
         } catch (error) {
-            return false
+            return false;
         }
     }
+
     function tempLogout() {
-        localStorage.removeItem("authData")
+        localStorage.removeItem("authData");
     }
+
     return (
         <div className="navbar">
-            <div className="dropdown">
-                <button className="dropbtn">
-                    <div className="div-icon"></div>
-                    <div className="div-icon"></div>
-                    <div className="div-icon"></div>
-                </button>
-                <div className="dropdown-content">
-                    <h2>
-                        Task Harbor
-                    </h2>
-                    {/* Not Signed In Navbar */}
-                    {!loginHelper() && (<>
-                        <li>
-                            <NavLink to="/">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/signup">
-                                Sign Up
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/signin">
-                                Login
-                            </NavLink>
-                        </li>
-                    </>)}
-                    {/* Signed In Navbar */}
-                    {loginHelper() && (<>
-                        <li >
-                            <NavLink to="/projectCreation">
-                                <div className="newProject">New Project</div>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/select-project">
-                                Projects
-                            </NavLink>
-                        </li>
-                        {/* List projects in Navbar */}
-                        {(() => {
-                            let projectsLinks = [];
-                            if (!projectsList) {
-                                return
-                            }
-                            projectsList['data'].forEach(project => {
-                                let urllink = "/projects/" + project.projectID
-                                var color = projectsList['color'][project.projectID]
-                                projectsLinks.push(<li key={project.projectID} className="projectlink"><NavLink to={urllink} ><div className="nameAndDot"><span style={{ backgroundColor: color }} className="dot" /> {project.projectName}</div></NavLink></li>)
-                            })
-                            return projectsLinks
-                        })()}
-                        <li>
-                            <NavLink to="/logout" onClick={tempLogout}>
-                                Logout
-                            </NavLink>
-                        </li>
-                    </>)}
-
-
-                </div>
+            <div className="navbar-header">
+                <h1>Task Harbor</h1>
+            </div>
+            <div className="navbar-content">
+                {!isLoggedIn() && (
+                    <>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/signup">Sign Up</NavLink>
+                        <NavLink to="/signin">Login</NavLink>
+                    </>
+                )}
+                {isLoggedIn() && (
+                    <>
+                        <NavLink to="/projectCreation" className="newProject">+ New Project</NavLink>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/select-project">Projects</NavLink>
+                        <div className="projects-list">
+                            {projectsList.map(project => {
+                                let urlLink = "/projects/" + project.projectID;
+                                var color = '#' + Math.floor(Math.random()*16777215).toString(16); 
+                                return (
+                                    <NavLink key={project.projectID} to={urlLink}>
+                                        <span className="dot" style={{ backgroundColor: color }}></span>
+                                        {project.projectName}
+                                    </NavLink>
+                                );
+                            })}
+                        </div>
+                        <NavLink to="/logout" onClick={tempLogout}>Logout</NavLink>
+                    </>
+                )}
             </div>
         </div>
-    )
+    );
 }
+
 export default NavBar;
